89c6a1c49a680a0900bc6a8d986985437f940de6
TINA MYERS <myers.tina515@gmail.com>
2020-12-14 18:30:30 -0800

    test

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const server = require('./server');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-server.start(process.env.PORT);
 
 // const MONGOOSE_URI = 'mongodb://localhost:27017/auth';
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(process.env.MONGODB_URI, options);
 
+
+server.start(process.env.PORT);
